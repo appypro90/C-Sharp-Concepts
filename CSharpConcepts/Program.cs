73d9f9432a3399cc9ad017a1468cbbc362c502c1
Arpita <appypro90@gmail.com>
2019-04-03 09:53:52 +0530

    Thread Concept branch has been created.

--- CSharpConcepts/Program.cs
+++ CSharpConcepts/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
+using System;
+using System.IO;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace CSharpConcepts
 {
@@ -10,6 +10,37 @@ namespace CSharpConcepts
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine(new Program().countCharacter());
+            Console.ReadLine();
+            //new Program().returnCount();
+
         }
+
+        private int countCharacter()
+        {
+            var character = 0;
+            //FileStream fileStream = new FileStream("C:\\Users\\grini\\OneDrive\\Documents\\AppyDocs.txt\\Git@appy.txt", FileMode.Open);
+            using (StreamReader rdr = new StreamReader("C:\\Users\\grini\\OneDrive\\Documents\\AppyDocs.txt\\Git@appy.txt"))
+            {
+                var count = rdr.ReadToEnd();
+                character = count.Length;
+                Thread.Sleep(4000);
+            }
+            return character;
+        }
+
+        //private async void returnCount()
+        //{
+        //    Task<int> task = new Task<int>(countCharacter);
+        //    task.Start();
+        //    var x = "Counting in progress...";
+        //    int reply = await task.
+        //    Console.WriteLine("The count is " + reply);
+
+
+        //}
+
+
     }
 }
