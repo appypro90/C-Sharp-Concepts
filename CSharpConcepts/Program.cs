3a343b7f69f57aede1b28060d223e7b4e39aba1a
Arpita <appypro90@gmail.com>
2019-04-04 09:21:05 +0530

    Types of variables and converting one type to another.

--- CSharpConcepts/Program.cs
+++ CSharpConcepts/Program.cs
@@ -9,7 +9,26 @@ namespace CSharpConcepts
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+                      
+            //string to int. returns boolean. If true returns result.
+            string myVariable = "45";
+            string anotherVariable = "Sen";
+            int number = 5;
+            Int32.TryParse(myVariable, out number);//manipulates number variable and returns that.
+            Int32.Parse(anotherVariable);
+            Console.WriteLine(number);
+
+            //int to string.
+            int numberX = 5;
+            Convert.ToString(numberX);
+            Console.WriteLine(numberX);
+
+
+
+
+           
+            Console.ReadLine();
         }
     }
 }
