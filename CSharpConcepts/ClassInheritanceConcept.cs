1468aaecee4ccf881711324916cbc7b8512972e8
Arpita <appypro90@gmail.com>
2019-04-06 15:34:27 +0530

    Various types of class implemented.

---
+++ CSharpConcepts/ClassInheritanceConcept.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts
+{
+    //It can be accessible from any assembly that refers it.
+    public class ClassInheritanceConcept
+    {
+    }
+
+    //Public class can be accessed, inherited from every project but can not be instantiated.
+    public abstract class BaseClass
+    {
+       private int Id { get; set; }
+       private string Name { get; set; }
+    }
+
+    //It is accessible only in this assembly and can not be inherited.
+    internal sealed class ChildClass : BaseClass
+    {
+        public int ChildID { get; set; }
+        public string ChildName { get; set; }
+    }
+
+
+}
--- CSharpConcepts/ClassMethodConcept.cs
+++ CSharpConcepts/ClassMethodConcept.cs
@@ -8,5 +8,6 @@ namespace CSharpConcepts
 {
     class ClassMethodConcept
     {
+
     }
 }
